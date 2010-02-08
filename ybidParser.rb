c80d5a77d10455db5774a3026a62ccdc7a22a1c4
Lee <liulijen@gmail.com>
2010-02-08 09:28:55 -0500

    Resolve coding to big5

--- ybidParser.rb
+++ ybidParser.rb
@@ -1,6 +1,11 @@
+#encoding: utf-8
 require 'nokogiri'
 require 'open-uri'
-html_doc = Nokogiri::HTML(open("http://tw.search.bid.yahoo.com/rss/searchrss?p=D700")).xpath("//body").each{|m|
-	puts m
-}
+require 'iconv'
+s1=open("http://tw.search.bid.yahoo.com/rss/search/?mac")
+s1.rewind
+puts  Iconv.conv("big5",s1.charset,s1.readlines.join("\n"))
+#html_doc = Nokogiri::HTML(open("http://tw.search.bid.yahoo.com/rss/search/?mac")).xpath("//body").each{|m|
+	#puts Iconv.conv("utf-8",m.charset,m)
+#}
 
